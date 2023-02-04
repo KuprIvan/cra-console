1d23e73277db03b7af42c0c7781a06a047eb5ba5
ivankupryianau <ivankupryianau@gmail.com>
2023-02-04 14:36:03 +0100

    developer/login popup udpate

--- src/Post/Post.tsx
+++ src/Post/Post.tsx
@@ -3,11 +3,14 @@ import React from 'react';
 const Post = () => {
     return (
         <div>
-          <h2>Post21</h2>
-          <h2>Post21</h2>
-          <h2>Post21</h2>
-          <h2>Post21</h2>
-          <h2>Post21</h2>
+            <h2>Post21</h2>
+            <h2>Post21</h2>
+            <h2>Post21</h2>
+            <h2>Post21</h2>
+            <h2>Post21</h2>
+            <div>New commit is here</div>
+            <div>New commit is here</div>
+            <div>New commit is here</div>
         </div>
     );
 };
