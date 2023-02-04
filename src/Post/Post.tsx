4d72df926dcf482e2a54b4b79f4de155d277a18d
ivankupryianau <ivankupryianau@gmail.com>
2023-02-04 14:31:47 +0100

    Test dev branch update

--- src/Post/Post.tsx
+++ src/Post/Post.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 const Post = () => {
     return (
         <div>
-          <h2>Post21</h2>
-          <h2>Post21</h2>
-          <h2>Post21</h2>
-          <h2>Post21</h2>
-          <h2>Post21</h2>
+         <div>Dev branch commit 1</div>
+         <div>Dev branch commit 2</div>
+         <div>Dev branch commit 3</div>
+         <div>Dev branch commit 4</div>
+         <div>Dev branch commit 5</div>
         </div>
     );
 };
